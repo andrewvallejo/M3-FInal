ac4cd85301ae389eb04166050e6a5781ef592982
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-10 11:11:57 -0600

    Add order call with error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,19 @@ export const getOrders = async() => {
       .then(response => response.json())
       .catch(err => console.error('Error fetching:', err));
 }
+
+export const postOrder = async(orderData) => {
+  return await fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify(orderData)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json
+      } else {
+          throw Error('Unable to submit order, try again later')   
+      }
+    })
+    .catch(err => console.error('Error Posting:', err));
+}
