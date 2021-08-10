058261066c4e3a2c3ad159e3b8a23dfaa8457408
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-10 10:26:04 -0600

    Update nanoid to be 10 to prevent matching keys

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -5,11 +5,11 @@ import './Orders.css';
 const Orders = ({orders} )=> {
   const orderEls = orders.map(order => {
     return (
-      <div key={nanoid(1)} className="order">
+      <div key={nanoid(10)} className="order">
         <h3>{order.name}</h3>
-        <ul  className="ingredient-list">
+        <ul  className="ingredient-list" key={nanoid(10)} >
           { order.ingredients.map(ingredient => {
-            return <li key={nanoid(1)}>{ingredient}</li>
+            return <li key={nanoid(10)}>{ingredient}</li>
           })}
         </ul>
       </div>
@@ -17,7 +17,7 @@ const Orders = ({orders} )=> {
   });
 
   return (
-    <section>
+    <section key={nanoid(10)}>
       { orderEls.length ? orderEls : <p>No orders yet!</p> }
     </section>
   )
