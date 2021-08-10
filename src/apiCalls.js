c719db044d34e6dab6fcbc7a78b42cfc6cbfa119
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-10 09:50:20 -0600

    Add async and awaits to apiCall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,5 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
+export const getOrders = async() => {
+  return await fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
+      .catch(err => console.error('Error fetching:', err));
 }
